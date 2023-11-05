195633135e263d6dacb55ef9e21de7c1e6cf440e
Yago <yago.ks5@gmail.com>
2023-11-05 13:53:54 -0300

    Adicionando modelo de cpu, velocidade cpu e tipo de sistema operacional

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,19 +1,25 @@
-const { stat } = require("node:fs");
+const { stat, cp } = require("node:fs");
 const os = require("node:os");
-
 setInterval(() => {
-  const { arch, platform, totalmem, freemem, cpus } = os;
+  const { arch, platform, totalmem, freemem, cpus, type } = os;
   const tRam = totalmem() / 1024 / 1024;
   const fRam = freemem() / 1024 / 1024;
   const usage = (fRam / tRam) * 100;
+  const cpuModel = cpus()[0].model;
+  const cpuSpeed = cpus()[0].speed / 1000;
+  const osType = type();
 
   const stats = {
     OS: platform(),
     Arch: arch(),
     TotalRam: `${parseInt(tRam)} MB`,
     FreeRam: `${parseInt(fRam)} MB`,
-    Usage: `${usage.toFixed(2)} %`,
-    //   Cpus: cpus(),
+    Usage: `A porcentagem de RAM livre no seu computador é de:  ${usage.toFixed(
+      2
+    )} %`,
+    Cpu: `O modelo de seu cpu é: ${cpuModel}`,
+    CpuSpeed: `A velocidade do seu cpu é de ${cpuSpeed} GHz`,
+    OSTYPE: osType,
   };
   console.clear();
   console.table(stats);
